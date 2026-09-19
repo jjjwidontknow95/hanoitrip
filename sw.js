a04170fc9a6af6881bc9a61f094e8e62d5e15424
jjjwidontknow95 <wjunwei95@gmail.com>
2026-09-20 01:36:25 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+var CACHE = "hanoi-v19";
+var FILES = [
+  "./", "./index.html", "./manifest.webmanifest",
+  "./icon-180.png", "./icon-192.png", "./icon-512.png"
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) {
+    return Promise.all(FILES.map(function (f) {
+      return c.add(f).catch(function () {});
+    }));
+  }).then(function () { return self.skipWaiting(); }));
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.map(function (k) {
+      if (k !== CACHE) { return caches.delete(k); }
+    }));
+  }).then(function () { return self.clients.claim(); }));
+});
+
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") { return; }
+  // 天氣 API 一律走網路，抓不到由 App 用上次存的結果，不要進快取
+  if (e.request.url.indexOf("api.open-meteo.com") >= 0) { return; }
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      if (hit) {
+        fetch(e.request).then(function (res) {
+          if (res && res.status === 200) {
+            caches.open(CACHE).then(function (c) { c.put(e.request, res.clone()); });
+          }
+        }).catch(function () {});
+        return hit;
+      }
+      return fetch(e.request).then(function (res) {
+        if (res && res.status === 200 && e.request.url.indexOf("http") === 0) {
+          var cl = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, cl); });
+        }
+        return res;
+      }).catch(function () { return caches.match("./index.html"); });
+    })
+  );
+});
